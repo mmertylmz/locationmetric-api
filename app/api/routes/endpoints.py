7ff5bd8772bd48803ca7bd5192c5d73d51612ff9
Mert Can Yılmaz <dokkensavatage@gmail.com>
2025-03-26 23:31:44 +0300

    - Added db configuration. - Added basic db test endpoint.

--- app/api/routes/endpoints.py
+++ app/api/routes/endpoints.py
@@ -1,11 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from app.db.database import get_db
+
 
 router = APIRouter()
 
-@router.get("/")
-async def read_root():
-    return {"message": "Welcome to the FastAPI application!"}
+# @router.get("/")
+# async def read_root():
+#     return {"message": "Welcome to the FastAPI application!"}
 
-@router.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "query": q}
+# @router.get("/items/{item_id}")
+# async def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "query": q}
+
+@router.get("/health/database")
+def check_database_connection(db: Session = Depends(get_db)):
+    try:
+        result = db.execute(text("SELECT 1"))
+        if result.scalar() == 1:
+            return {"status": "ok", "message": "Database connection successful"}
+        return {"status": "error", "message": "Unknown database error"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.core.config import Config
 
-DATABASE_URL = "sqlite:///./test.db"  # Update this with your actual database URL
+engine = create_engine(
+    Config.DATABASE_URL, 
+    echo=True,
+    connect_args={"TrustServerCertificate": "yes"}
+    )
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
